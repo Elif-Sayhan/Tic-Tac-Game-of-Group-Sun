00dada1de413ae558a7fa665f2b1e8a31e5f76b6
Ilona Baude <ilona.baude@laplateforme.io>
2024-10-25 14:56:16 +0200

    game visual added

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
 board = ["-","-","-",
          "-","-","-",
          "-","-","-"]
+
+
+#Game Visuals 
+
+#Game Start screen 
+def game_start_screen(word):
+    top_ = " /)__/)"+" "*2 +"*" * (len(word) + 4) + "(\\__(\\"+" "*3
+    middle = "(='.'=)"+" "*2 +"* " + word + " *" + "(='.'=)"+" "*2
+    bottom_ = "('')_('')"+"*" * (len(word) + 4) + "('')_('')"
+    print(top_)
+    print(middle)
+    print(bottom_)
+
+
+#Game Victory Screen 
+def victory_screen(word,w):
+    top_ = " /)__/)"+" "*2 +"*" * (len(word) + 4) + "(\\__(\\"+" "*3
+    middle = "(='.'=)"+" "*2 +"* " + word + " *" + "(='.'=)"+" "*2
+    bottom_ = "('')_('')"+"*" * (len(word) + 4) + "('')_('')"
+    if w == "win":
+        middle = middle[:3] + "w" + middle[3+1:]
+        middle = middle[:len(word)+16] + "w" + middle[len(word)+17:]
+    elif w == "lose":
+        middle = middle[:3] + "^" + middle[3+1:]
+        middle = middle[:len(word)+16] + "^" + middle[len(word)+17:]
+    print(top_)
+    print(middle)
+    print(bottom_)
